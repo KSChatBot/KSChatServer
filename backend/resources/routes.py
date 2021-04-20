806ac0df299ace45ba4f2200708612aeb3d6ba28
KSChatBot <saintphs@gmail.com>
2021-04-20 17:35:06 +0900

    add_weather_function

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,14 +1,14 @@
 from .api_content import API_Content_NS
 from .auth import Authentication_NS
 from .reset_password import Password_NS
-from .weather import WeaherInfo_NS
+from .weather import WeatherInfo_NS
 
 def initialize_routes(api):
     api.add_namespace(API_Content_NS, '/api/contents')
 
     api.add_namespace(Authentication_NS, '/accounts')
 
-    api.add_namespace(WeaherInfo_NS, '/api/weather')
+    api.add_namespace(WeatherInfo_NS, '/api/weather')
 
     api.add_namespace(Password_NS, '/api/auth')
     # api.add_resource(ForgotPassword, '/api/auth/forgot')
--- backend/resources/weather.py
+++ backend/resources/weather.py
@@ -6,13 +6,14 @@ import datetime
 from flask import Response, request, json, jsonify
 from flask_restx import Resource, Namespace, fields
 from database.models import API_Content, User
+import services.weather_info as wf
 
-WeaherInfo_NS = Namespace(
-    name="WeaherInfo",
+WeatherInfo_NS = Namespace(
+    name="WeatherInfo",
     description="날씨 정보를 공공데이터나 네이버로 보터 가져오는 API.",
 )
 
-weaherinfo_fields = WeaherInfo_NS.model('WeaherInfo', {  # Model 객체 생성
+weaherinfo_fields = WeatherInfo_NS.model('WeatherInfo', {  # Model 객체 생성
     'api_name': fields.String(description='a API_Content', required=True, example="날씨정보"),
     'sido': fields.String(description='날씨조회를 위한 시도명'),
     'gungu': fields.String(description='날씨조회를 위한 시군구명'),
@@ -20,12 +21,11 @@ weaherinfo_fields = WeaherInfo_NS.model('WeaherInfo', {  # Model 객체 생성
 })
 
 
-@WeaherInfo_NS.route('/get_naver')
-# /<string:callback_url><string:sido><string:gungu><string:dong>
-class WeaherInfoNaver(Resource):
-    @WeaherInfo_NS.expect(weaherinfo_fields)
-    @WeaherInfo_NS.response(200, 'Success')
-    @WeaherInfo_NS.response(500, 'Failed')
+@WeatherInfo_NS.route('/get_naver')
+class WeatherInfoNaver(Resource):
+    @WeatherInfo_NS.expect(weaherinfo_fields)
+    @WeatherInfo_NS.response(200, 'Success')
+    @WeatherInfo_NS.response(500, 'Failed')
     def post(self):
         print('네이버 날씨 크롤링 시작!')
         try:
@@ -96,4 +96,88 @@ class WeaherInfoNaver(Resource):
             }, 200
 
         except:
-            print("WeaherInfoNaver Exception error!")
+            print("WeatherInfoNaver Exception error!")
+
+@WeatherInfo_NS.route('/get_public_data')
+class WeatherInfoPublicData(Resource):
+    @WeatherInfo_NS.expect(weaherinfo_fields)
+    @WeatherInfo_NS.response(200, 'Success')
+    @WeatherInfo_NS.response(500, 'Failed')
+    def post(self):
+        print('공공데이터 동네예보조회 시작!')
+        try:
+            data_json = request.data
+            data_dict = json.loads(data_json)
+            print("======공공데이터 동네예보조회정보=======")
+            print(data_dict)
+            print("======end 공공데이터 동네예보조회정보=======")
+
+            api_name = data_dict.get('api_name')
+            apicontent = API_Content.objects.get(api_name=api_name)
+            sido = data_dict.get('sido')
+            gungu = data_dict.get('gungu')
+            dong = data_dict.get('dong')
+
+            XY_data = wf.get_weather_location_XY(sido, gungu, dong)
+            nx = XY_data[0]
+            ny = XY_data[1]
+            loc_name = XY_data[2]
+
+            if ((nx < 0) or (ny < 0)):
+                return {}, 404
+
+            # api_endpoint = 'https://search.naver.com/search.naver?'
+            # payload = "query=" + sido + '+' + gungu + '+' + dong + '+' + "날씨"
+            # print("네이버날씨URL:{}".format(callback_url + payload))
+            # api_endpoint
+            # api_key
+            # 동네예보조회 날씨 API 호출 처리를 위한 입력값 정의(Payload)
+            date = wf.get_rpt_time()
+
+            #     "numOfRows=10" + "&" +\
+            #     "pageNo=1" + "&" +\
+
+            payload = "serviceKey=" + apicontent.api_key + "&" +\
+                "dataType=JSON" + "&" +\
+                "numOfRows=10" + "&" +\
+                "pageNo=1" + "&" +\
+                "base_date=" + date[0] + "&" +\
+                "base_time=" + date[1] + "&" +\
+                "nx=" + str(nx) + "&" +\
+                "ny=" + str(ny)
+
+            # 동네예보조회 날씨 API 호출결과  처리 - 정상적 호출결과인 Json형태 여부 체크
+            res = req.get(apicontent.api_endpoint + payload)
+
+            if 'json' in res.headers.get('Content-Type'):
+                data = json.loads(res.text)
+            else:
+                print('Response content is not in JSON format.')
+                return {}, 404
+
+            # 판다스이용 테이블 자료로 변환
+            df_res = pd.DataFrame(data['response']['body']['items']['item'])
+
+            # 출력 결과를 JSON으로 가공 출력
+            result = {}
+            result["현재위치"] =  loc_name    
+            for index, row in df_res.iterrows():
+                print("\t", wf.w_items[str(row['category'])][0], " : ", wf.get_real_value(row['category'], row['fcstValue']), end = "")
+                result[wf.w_items[str(row['category'])][0]] =  wf.get_real_value(row['category'], row['fcstValue'])    
+                if wf.w_items[str(row['category'])][1]  != "":
+                    result[wf.w_items[str(row['category'])][0]] =  wf.get_real_value(row['category'], row['fcstValue']) + wf.w_items[str(row['category'])][1] 
+                    print(wf.w_items[str(row['category'])][1])
+                else:
+                    print()
+
+            print('공공데이터 동네예보조회 종료!')
+
+            # Serializing json
+            # result_json_object = json.dumps(result, indent = 4, ensure_ascii = False)  
+
+            # return result_json_object, 200
+            # return json.dumps(result, indent = 4, ensure_ascii = False), 200
+            return result, 200
+
+        except Exception as e:
+            print("WeatherInfoPublicData Exception error! ==> ",e)
